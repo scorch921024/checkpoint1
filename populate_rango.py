6ba6013b49817fa7be5840f6508ada2808861534
scorch921024 <no13scorch@gmail.com>
2017-02-16 09:56:30 +0000

    Add files via upload

--- populate_rango.py
+++ populate_rango.py
@@ -1,8 +1,9 @@
 import os
-os.environ.setdefault('DJANGO_SETTINGS_MODULE','tango_with_django_project.settings')
-
+os.environ.setdefault('DJANGO_SETTINGS_MODULE',
+                      'tango_with_django_project.settings')
 import django
 django.setup()
+
 from rango.models import Category, Page
 
 def populate():
